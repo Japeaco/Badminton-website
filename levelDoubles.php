01384afd2a052eaa7312cf11915f2ee24a7478ce
James Peacock <32509558+Japeaco@users.noreply.github.com>
2019-07-01 12:03:56 +0100

    Update levelDoubles.php

--- levelDoubles.php
+++ levelDoubles.php
@@ -1,72 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Badminton Home</title>
+	<title>Level Doubles</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="badmintonStyle.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="badmintonJS.js"></script>
+	<script src="javascript.js" defer="true"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<style>
+    #levelDoublesItem {
+        color:#006680;
+    }
+    </style>
 </head>
 <body>
 
+<div class="wrapper">
+
 <!-- side navigation -->
-<nav class="nav">
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" id="topList" href="home.php">Bridlington and District Badminton association</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="home.php">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="join.php">Join</a>
-    </li>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent">
-	  Current League Tables
-	</button>
-	<div class="collapse" id="navbarToggleExternalContent">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="levelDoubles.php" style="color:#006680">Level Doubles</a>
-        </li>
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="mixedFours.php">Mixed Fours</a>
-        </li>
-      </ul>
-    </div>  
-	<li class="nav-item">
-      <a class="nav-link" href="previousLeagueTables.php">Previous League Tables</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="matches.php">Matches</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="leagueRules.php">League Rules</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="scoresheets.php">Score Sheets</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="tournamentData.php">Tournament Dates</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="racketServices.php">Racket Stringing Services</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="events.php">Events</a>
-    </li>
-  </ul>
+<nav class="nav bg-dark" id="sidebar">
+    <div class="sideNavigationBar" id="sideNavigationBar"></div>
 </nav> 
+
+<div class="collapseButton">
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <div class="container-fluid">
+        <button type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fa fa-bars"></i>
+        </button>
+    </div>
+</nav>
+</div>
 
 <div class="main">
 
-<h1 class="titleText">Level Doubles</h1>
+<h1 class="titleText">Level Doubles 2019-2020 Season</h1>
 
 <div class="mx-auto embed-responsive embed-responsive-16by9" style="width:510px ; height:300px">
   <iframe class="embed-responsive-item" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSYEdOaN-pEzphAV5EPZgVkKJ_WRFFrUFhbRwtxjy98f8b9ozOmt_fKvMoPZ3PIWqc1JnX3FmOggs05/pubhtml?gid=728064575&chrome=false&widget=false&range=LevelsDiv1&single=true&headers=false">
@@ -83,6 +55,9 @@
   </iframe>
 </div>
 
+<div class="bottomFooter" id="bottomFooter"></div>
+
+</div>
 </div>
 
 </body>
