8c8246a8e5f9ff2e2c58dcaa78aa50bbca3ad777
James Peacock <32509558+Japeaco@users.noreply.github.com>
2019-07-01 12:05:44 +0100

    Update previousLeagueTables.php

--- previousLeagueTables.php
+++ previousLeagueTables.php
@@ -1,80 +1,50 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Badminton Home</title>
+	<title>Previous League Tables</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="badmintonStyle.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="badmintonJS.js"></script>
+	<script src="javascript.js" defer="true"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<style>
+    #previousItem {
+        color:#006680;
+    }
+    </style>
 </head>
 <body>
 
+<div class="wrapper">
+
 <!-- side navigation -->
-<nav class="nav">
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" id="topList" href="home.php">Bridlington and District Badminton association</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="home.php">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="join.php">Join</a>
-    </li>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent">
-	  Current League Tables
-	</button>
-	<div class="collapse" id="navbarToggleExternalContent">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="levelDoubles.php">Level Doubles</a>
-        </li>
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="mixedFours.php">Mixed Fours</a>
-        </li>
-      </ul>
-    </div>
-	<li class="nav-item">
-      <a class="nav-link" href="previousLeagueTables.php" style="color:#006680">Previous League Tables</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="matches.php">Matches</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="leagueRules.php">League Rules</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="scoresheets.php">Score Sheets</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="tournamentData.php">Tournament Dates</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="racketServices.php">Racket Stringing Services</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="events.php">Events</a>
-    </li>
-  </ul>
+<nav class="nav bg-dark" id="sidebar">
+    <div class="sideNavigationBar" id="sideNavigationBar"></div>
 </nav> 
+
+<div class="collapseButton">
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <div class="container-fluid">
+        <button type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fa fa-bars"></i>
+        </button>
+    </div>
+</nav>
+</div>
 
 <div class="main">
 
 <h1 class="titleText">Previous League Tables</h1>
 
 <div class="homeText">
-  <h3 class="previousLeaguesHeader">League Results - End of 2018 Season</h3>
+  <h3 class="previousLeaguesHeader">League Results - End of 2018/19 Season</h3>
 </div>
 
 <div class="container">
-<div class="row">
-  <div class="col-sm-6" style="">
     <div class="indTable">
     <h5 class="tableHeader mx-auto">Levels League Division 1</h5>
     <table class="table table-borderless table-sm mx-auto">
@@ -91,95 +61,60 @@
 	</thead>
 	<tbody>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Priory 1</td>
+		<td>15</td>
+	    <td>15</td>
 		<td>0</td>
 		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>0</td>
+		<td>174</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Scarborough</td>
+		<td>15</td>
+	    <td>11</td>
 		<td>0</td>
+		<td>4</td>
 		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>133</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">High Flyers 1</td>
+		<td>15</td>
+	    <td>8</td>
 		<td>0</td>
+		<td>7</td>
 		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>103</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
+	    <td style="text-align:left">BBC 1</td>
+		<td>15</td>
+	    <td>7</td>
 		<td>0</td>
 		<td>8</td>
-		<td>32</td>
+		<td>0</td>
+		<td>91</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Northcliffe 1</td>
+		<td>15</td>
+	    <td>4</td>
 		<td>0</td>
+		<td>11</td>
 		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>73</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Priory 2</td>
+		<td>15</td>
+	    <td>0</td>
 		<td>0</td>
+		<td>15</td>
 		<td>0</td>
-		<td>8</td>
 		<td>32</td>
       </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
+	  
 	</tbody>
 	</table>
 	</div>
@@ -199,95 +134,60 @@
 	</thead>
 	<tbody>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Acorn 1</td>
+		<td>15</td>
+	    <td>14</td>
 		<td>0</td>
+		<td>1</td>
 		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>157</td>
       </tr>
 	  <tr>
 	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+		<td>15</td>
+	    <td>9</td>
+		<td>1</td>
+		<td>5</td>
 		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>132</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Northcliffe 2</td>
+		<td>15</td>
+	    <td>9</td>
+		<td>1</td>
+		<td>5</td>
 		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>123</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Priory 3</td>
+		<td>15</td>
+	    <td>9</td>
 		<td>0</td>
+		<td>6</td>
 		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>116</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Acorn 2</td>
+		<td>15</td>
+	    <td>3</td>
 		<td>0</td>
+		<td>12</td>
 		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>42</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">High Flyers 2</td>
+		<td>15</td>
+	    <td>0</td>
 		<td>0</td>
+		<td>15</td>
 		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>21</td>
       </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
+	  
 	</tbody>
 	</table>
 	</div>
@@ -307,100 +207,63 @@
 	</thead>
 	<tbody>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">BBC 3</td>
+		<td>10</td>
+	    <td>8</td>
+		<td>1</td>
+		<td>1</td>
 		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>99</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
+	    <td style="text-align:left">Northcliffe 3</td>
+		<td>10</td>
+	    <td>8</td>
+		<td>0</td>
 		<td>2</td>
-	    <td>2</td>
 		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>95</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
+	    <td style="text-align:left">Acorn 3</td>
+		<td>10</td>
+	    <td>4</td>
 		<td>2</td>
-	    <td>2</td>
+		<td>4</td>
 		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>59</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Priory 4</td>
+		<td>10</td>
+	    <td>4</td>
+		<td>1</td>
+		<td>5</td>
 		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>57</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">High Flyers 4</td>
+		<td>10</td>
+	    <td>0</td>
+		<td>3</td>
+		<td>7</td>
 		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>48</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
+	    <td style="text-align:left">High Flyers 3</td>
+		<td>10</td>
 	    <td>2</td>
+		<td>1</td>
+		<td>7</td>
 		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>39</td>
       </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
+	  
 	</tbody>
 	</table>
-	</div>
-	</div>
-  <div class="col-sm-6">
+  </div>
   <div class="indTable">
   <h5 class="tableHeader mx-auto">Mixed Fours Division 1</h5>
     <table class="table table-borderless table-sm mx-auto">
@@ -417,95 +280,60 @@
 	</thead>
 	<tbody>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Priory A</td>
+		<td>10</td>
+	    <td>10</td>
 		<td>0</td>
 		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>0</td>
+		<td>115</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
+	    <td style="text-align:left">Acorn A</td>
+		<td>10</td>
+	    <td>7</td>
+		<td>1</td>
 		<td>2</td>
-	    <td>2</td>
 		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>90</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Northcliffe A</td>
+		<td>10</td>
+	    <td>4</td>
+		<td>3</td>
+		<td>3</td>
 		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>76</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
+	    <td style="text-align:left">Albrough A</td>
+		<td>10</td>
+	    <td>3</td>
 		<td>2</td>
-	    <td>2</td>
+		<td>5</td>
 		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>52</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
+	    <td style="text-align:left">Priory B</td>
+		<td>10</td>
 	    <td>2</td>
 		<td>0</td>
-		<td>0</td>
 		<td>8</td>
-		<td>32</td>
+		<td>0</td>
+		<td>37</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
+	    <td style="text-align:left">Northcliffe B</td>
+		<td>10</td>
+	    <td>1</td>
 		<td>0</td>
+		<td>9</td>
 		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>27</td>
       </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
+	  
 	  </tbody>
 	 </table>
 	 </div>
@@ -525,102 +353,61 @@
 	</thead>
 	<tbody>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
+	    <td style="text-align:left">Acorn B</td>
 		<td>8</td>
-		<td>32</td>
+	    <td>6</td>
+		<td>0</td>
+		<td>2</td>
+		<td>0</td>
+		<td>77</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
+	    <td style="text-align:left">BBC A</td>
 		<td>8</td>
-		<td>32</td>
+	    <td>7</td>
+		<td>0</td>
+		<td>1</td>
+		<td>0</td>
+		<td>67</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
+	    <td style="text-align:left">Northcliffe C</td>
 		<td>8</td>
-		<td>32</td>
+	    <td>4</td>
+		<td>0</td>
+		<td>4</td>
+		<td>0</td>
+		<td>52</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
+	    <td style="text-align:left">Acorn C</td>
+		<td>8</td>
 	    <td>2</td>
 		<td>0</td>
+		<td>6</td>
 		<td>0</td>
-		<td>8</td>
-		<td>32</td>
+		<td>44</td>
       </tr>
 	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
+	    <td style="text-align:left">Acorn D</td>
 		<td>8</td>
-		<td>32</td>
+	    <td>1</td>
+		<td>0</td>
+		<td>7</td>
+		<td>0</td>
+		<td>30</td>
       </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
-	  <tr>
-	    <td style="text-align:left">BBC 2</td>
-		<td>2</td>
-	    <td>2</td>
-		<td>0</td>
-		<td>0</td>
-		<td>8</td>
-		<td>32</td>
-      </tr>
+	  
 	</tbody>
 	</table>
 	</div>
-  </div>
 </div> 
+
+<div class="bottomFooter" id="bottomFooter"></div>
+
 </div>
 
+</div>
 </div>
 
 </body>
