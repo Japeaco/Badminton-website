22ae8713611a369c56be4006f7c9782660746e90
James Peacock <32509558+Japeaco@users.noreply.github.com>
2019-07-01 12:00:48 +0100

    Add files via upload

---
+++ dreamTeams.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dream Teams</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="badmintonStyle.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="javascript.js" defer="true"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<style>
+    #dreamTeamsItem {
+        color:#006680;
+    }
+    </style>
+</head>
+<body>
+    
+<div class="wrapper">
+
+<!-- side navigation -->
+<nav class="nav bg-dark" id="sidebar">
+    <div class="sideNavigationBar" id="sideNavigationBar"></div>
+</nav> 
+
+<div class="collapseButton">
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <div class="container-fluid">
+        <button type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fa fa-bars"></i>
+        </button>
+    </div>
+</nav>
+</div>
+
+<div class="main">
+
+<h1 class="titleText">Dream Teams - Players with Highest Rankings</h1>
+
+<div class="mx-auto embed-responsive embed-responsive-16by9" style="width:730px; height:350px">
+  <iframe class="embed-responsive-item" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS98f8RuToFYq1Tl-fAypMQQa9TEsVFU_c9khmqXEtdswK_cPKaLXkaZBc98AQeOAK7hk2QUNNAXzgU/pubhtml?gid=728064575&amp;chrome=false&amp;widget=false&amp;range=DreamLevelsDiv1&amp;single=true&amp;widget=true&amp;headers=false">
+  </iframe>
+</div>
+
+<div class="mx-auto embed-responsive embed-responsive-16by9" style="width:730px; height:350px">
+  <iframe class="embed-responsive-item" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS98f8RuToFYq1Tl-fAypMQQa9TEsVFU_c9khmqXEtdswK_cPKaLXkaZBc98AQeOAK7hk2QUNNAXzgU/pubhtml?gid=728064575&amp;chrome=false&amp;widget=false&amp;range=DreamLevelsDiv2&amp;single=true&amp;widget=true&amp;headers=false">
+  </iframe>
+</div>
+
+<div class="mx-auto embed-responsive embed-responsive-16by9" style="width:730px; height:350px;">
+  <iframe class="embed-responsive-item" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS98f8RuToFYq1Tl-fAypMQQa9TEsVFU_c9khmqXEtdswK_cPKaLXkaZBc98AQeOAK7hk2QUNNAXzgU/pubhtml?gid=728064575&amp;chrome=false&amp;widget=false&amp;range=DreamLevelsDiv3&amp;single=true&amp;widget=true&amp;headers=false">
+  </iframe>
+</div>
+
+<div class="mx-auto embed-responsive embed-responsive-16by9" style="width:730px; height:350px">
+  <iframe class="embed-responsive-item" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS98f8RuToFYq1Tl-fAypMQQa9TEsVFU_c9khmqXEtdswK_cPKaLXkaZBc98AQeOAK7hk2QUNNAXzgU/pubhtml?gid=728064575&amp;chrome=false&amp;widget=false&amp;range=DreamLevelsDiv4&amp;single=true&amp;widget=true&amp;headers=false">
+  </iframe>
+</div>
+
+<div class="mx-auto embed-responsive embed-responsive-16by9" style="width:730px; height:350px">
+  <iframe class="embed-responsive-item" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS98f8RuToFYq1Tl-fAypMQQa9TEsVFU_c9khmqXEtdswK_cPKaLXkaZBc98AQeOAK7hk2QUNNAXzgU/pubhtml?gid=728064575&amp;chrome=false&amp;widget=false&amp;range=DreamFoursDiv1&amp;single=true&amp;widget=true&amp;headers=false">
+  </iframe>
+</div>
+
+<div class="mx-auto embed-responsive embed-responsive-16by9" style="width:730px; height:350px">
+  <iframe class="embed-responsive-item" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS98f8RuToFYq1Tl-fAypMQQa9TEsVFU_c9khmqXEtdswK_cPKaLXkaZBc98AQeOAK7hk2QUNNAXzgU/pubhtml?gid=728064575&amp;chrome=false&amp;widget=false&amp;range=DreamFoursDiv2&amp;single=true&amp;widget=true&amp;headers=false">
+  </iframe>
+</div>
+
+<div class="bottomFooter" id="bottomFooter"></div>
+
+</div>
+
+</div>
+
+</body>
+</html>
