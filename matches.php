5799b6467ca60a5f76e49df159f21576ddd294fc
James Peacock <32509558+Japeaco@users.noreply.github.com>
2019-07-01 12:04:48 +0100

    Update matches.php

--- matches.php
+++ matches.php
@@ -8,61 +8,33 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="badmintonJS.js"></script>
+	<script src="javascript.js" defer="true"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<style>
+    #matchesItem {
+        color:#006680;
+    }
+    </style>
 </head>
 <body>
+    
+<div class="wrapper">
 
 <!-- side navigation -->
-<nav class="nav">
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" id="topList" href="home.php">Bridlington and District Badminton association</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="home.php">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="join.php">Join</a>
-    </li>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent">
-	  Current League Tables
-	</button>
-	<div class="collapse" id="navbarToggleExternalContent">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="levelDoubles.php">Level Doubles</a>
-        </li>
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="mixedFours.php">Mixed Fours</a>
-        </li>
-      </ul>
-    </div>  
-	<li class="nav-item">
-      <a class="nav-link" href="previousLeagueTables.php">Previous League Tables</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="matches.php" style="color:#006680">Matches</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="leagueRules.php">League Rules</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="scoresheets.php">Score Sheets</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="tournamentData.php">Tournament Dates</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="racketServices.php">Racket Stringing Services</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="events.php">Events</a>
-    </li>
-  </ul>
+<nav class="nav bg-dark" id="sidebar">
+    <div class="sideNavigationBar" id="sideNavigationBar"></div>
 </nav> 
+
+<div class="collapseButton">
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <div class="container-fluid">
+        <button type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fa fa-bars"></i>
+        </button>
+    </div>
+</nav>
+</div>
 
 <div class="main">
 
@@ -73,6 +45,9 @@
   </iframe>
 </div>
 
+<div class="bottomFooter" id="bottomFooter"></div>
+
+</div>
 </div>
 
 </body>
