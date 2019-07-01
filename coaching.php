32ce43e4a359f6c7d6d8ca925233401af5a1e66e
James Peacock <32509558+Japeaco@users.noreply.github.com>
2019-07-01 11:56:47 +0100

    Add files via upload

---
+++ coaching.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>League Rules</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="badmintonStyle.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="javascript.js" defer="true"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<style>
+    #coachingItem {
+        color:#006680;
+    }
+    </style>
+</head>
+<body>
+    
+<div class="wrapper">
+
+<!-- side navigation -->
+<nav class="nav bg-dark" id="sidebar">
+    <div class="sideNavigationBar" id="sideNavigationBar"></div>
+</nav> 
+
+<div class="collapseButton">
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <div class="container-fluid">
+        <button type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fa fa-bars"></i>
+        </button>
+    </div>
+</nav>
+</div>
+
+<div class="main">
+
+<h1 class="titleText">Coaching</h1>
+
+<div class="homeText">
+  <p>Find out how to improve your game with one-on-one or group coaching events.  Run by an experienced Yorkshire Badminton League player, these sessions will help you to identify aspects of your game that require improvement, allow you to practise your strokes and help you with strategy.</p><p>Sessions are expected to start in September 2019. For further information, contact Paul Robinson on 07952 915013.</p>
+
+</div>
+
+<div class="bottomFooter" id="bottomFooter"></div>
+
+</div>
+</div>
+
+</body>
+</html>
