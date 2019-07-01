f07cb2df33f72883f5e233c765bc7da452dd7b16
James Peacock <32509558+Japeaco@users.noreply.github.com>
2019-07-01 12:06:35 +0100

    Update scoresheets.php

--- scoresheets.php
+++ scoresheets.php
@@ -1,76 +1,49 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Badminton Home</title>
+	<title>Scoresheets</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="badmintonStyle.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="badmintonJS.js"></script>
+	<script src="javascript.js" defer="true"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<style>
+    #scoresheetsItem {
+        color:#006680;
+    }
+    </style>
 </head>
 <body>
+    
+<div class="wrapper">
 
 <!-- side navigation -->
-<nav class="nav">
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" id="topList" href="home.php">Bridlington and District Badminton association</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="home.php">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="join.php">Join</a>
-    </li>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent">
-	  Current League Tables
-	</button>
-	<div class="collapse" id="navbarToggleExternalContent">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="levelDoubles.php">Level Doubles</a>
-        </li>
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="mixedFours.php">Mixed Fours</a>
-        </li>
-      </ul>
-    </div>  
-	<li class="nav-item">
-      <a class="nav-link" href="previousLeagueTables.php">Previous League Tables</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="matches.php">Matches</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="leagueRules.php">League Rules</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="scoresheets.php" style="color:#006680">Score Sheets</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="tournamentData.php">Tournament Dates</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="racketServices.php">Racket Stringing Services</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="events.php">Events</a>
-    </li>
-  </ul>
+<nav class="nav bg-dark" id="sidebar">
+    <div class="sideNavigationBar" id="sideNavigationBar"></div>
 </nav> 
+
+<div class="collapseButton">
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <div class="container-fluid">
+        <button type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fa fa-bars"></i>
+        </button>
+    </div>
+</nav>
+</div>
 
 <div class="main">
 
 <h1 class="titleText">Score Sheets</h1>
 
 <div class="homeText">
-  <h3 class="homeHeader">Score sheets for use during matches can be downloaded here</h3>
-  <p>Babble about the score sheet</p>
+    
+  <h3 class="homeHeader">Important - Instructions for Score sheets for the 2019/2020 Season</h3>
+  <p>See below for links to download the latest score sheets. These should be comleted at the end of each match and signed by both team captains. It is the responsibility of the home team captain to ensure that this is done correctly.</p><p>For the first set of games, please write the full name of the player with first and last name. Subsequent names can be written as first names only if preferred, but the full name of every player must appear once on every score sheet.</p><p>After the match, please send images of the completed score sheets to the following email address, no later than 7pm on the Friday following the match.</p><p>bdbascoresheets@gmail.com</p><p>Please note that teams may receive penalty points if sheets are illegible or if they are received late.</p><br><br>
   <a href="https://drive.google.com/file/d/0B8Us6QJmAcL5VGFjVFNVZTdNUDBPelNEdmVBdkR5TTRfYTVF/view">Level Doubles form download link</a>
   <a href="LevelDoubles.pdf" download>
     <i class="fas fa-file-download"></i><br>
@@ -79,9 +52,11 @@
   <a href="MixedFours.pdf" download>
     <i class="fas fa-file-download"></i><br>
   </a>
-  <img src="form.jpg" class="mx-auto d-block">
 </div>
 
+<div class="bottomFooter" id="bottomFooter"></div>
+
+</div>
 </div>
 
 </body>
