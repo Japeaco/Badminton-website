afde492c18029220bfa7e205a9699c199c361b97
James Peacock <32509558+Japeaco@users.noreply.github.com>
2019-07-01 12:06:13 +0100

    Update racketServices.php

--- racketServices.php
+++ racketServices.php
@@ -1,79 +1,57 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Badminton Home</title>
+	<title>Racket Services</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="badmintonStyle.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="badmintonJS.js"></script>
+	<script src="javascript.js" defer="true"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<style>
+    #racketServicesItem {
+        color:#006680;
+    }
+    </style>
 </head>
 <body>
+    
+<div class="wrapper">
 
 <!-- side navigation -->
-<nav class="nav">
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" id="topList" href="home.php">Bridlington and District Badminton association</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="home.php">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="join.php">Join</a>
-    </li>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent">
-	  Current League Tables
-	</button>
-	<div class="collapse" id="navbarToggleExternalContent">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="levelDoubles.php">Level Doubles</a>
-        </li>
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="mixedFours.php">Mixed Fours</a>
-        </li>
-      </ul>
-    </div>  
-	<li class="nav-item">
-      <a class="nav-link" href="previousLeagueTables.php">Previous League Tables</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="matches.php">Matches</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="leagueRules.php">League Rules</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="scoresheets.php">Score Sheets</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="tournamentData.php">Tournament Dates</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="racketServices.php" style="color:#006680">Racket Stringing Services</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="events.php">Events</a>
-    </li>
-  </ul>
+<nav class="nav bg-dark" id="sidebar">
+    <div class="sideNavigationBar" id="sideNavigationBar"></div>
 </nav> 
+
+<div class="collapseButton">
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <div class="container-fluid">
+        <button type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fa fa-bars"></i>
+        </button>
+    </div>
+</nav>
+</div>
 
 <div class="main">
 
 <h1 class="titleText">Racket Stringing Services</h1>
-
+<div class="racketText">
+<img src="racket.jpg" class="mx-auto d-block" id="racketImage">
+  <p>Eventually, your racket will need restringing; the tension reduces over time and strings can break. If they do break, they can place a lot of strain on a badminton racket frame  possibly causing it to distort permanently. As quickly as you can after a string has broken, we recommend cutting through the rest with a knife or scissors to avoid possible damage to your racket.</p>
+  <p>We can offer re-stringing services locally with a variety of string types and a tension of your choosing. The standard strings available are Yonex BG65 or Gosen Pro70, and the total charge for restringing your racket using either of these strings will be £15.</p> 
+    <p>Alternatively, you can specify a string to be ordered in specially and the charge may vary depending on the string chosen. Another option would be to provide your own string and the charge will then be £10 per racket.</p>
+</div>
 <div class="homeText">
-  <p>Eventually, your racket will need restringing; the tension reduces over time and strings can break. 
-     We can offer re-stringing services locally with a variety of string types and a tension of your choosing.
-     Contact John on 01262 and offer a shitload of money.</p>
+     <p>For further details, please contact Leo on 07527 272823.</p>
 </div>
 
+<div class="bottomFooter" id="bottomFooter"></div>
+
+</div>
 </div>
 
 </body>
