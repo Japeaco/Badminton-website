ed2a992ed707e98c2583bec2066ad388feb349db
James Peacock <32509558+Japeaco@users.noreply.github.com>
2019-07-01 12:03:02 +0100

    Update join.php

--- join.php
+++ join.php
@@ -1,68 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Badminton Home</title>
+	<title>Join</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="badmintonStyle.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="badmintonJS.js"></script>
+	<script src="javascript.js" defer="true"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<style>
+    #joinItem {
+        color:#006680;
+    }
+    </style>
 </head>
 <body>
+    
+<div class="wrapper">
 
 <!-- side navigation -->
-<nav class="nav">
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" id="topList" href="home.php">Bridlington and District Badminton association</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="home.php">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="join.php" style="color:#006680">Join</a>
-    </li>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent">
-	  Current League Tables
-	</button>
-	<div class="collapse" id="navbarToggleExternalContent">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="levelDoubles.php">Level Doubles</a>
-        </li>
-        <li class="nav-item">
-          <a id="sideCollapsed" class="nav-link" href="mixedFours.php">Mixed Fours</a>
-        </li>
-      </ul>
-    </div>  
-	<li class="nav-item">
-      <a class="nav-link" href="previousLeagueTables.php">Previous League Tables</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="matches.php">Matches</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="leagueRules.php">League Rules</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="scoresheets.php">Score Sheets</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="tournamentData.php">Tournament Dates</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="racketServices.php">Racket Stringing Services</a>
-    </li>
-	<li class="nav-item">
-      <a class="nav-link" href="events.php">Events</a>
-    </li>
-  </ul>
+<nav class="nav bg-dark" id="sidebar">
+    <div class="sideNavigationBar" id="sideNavigationBar"></div>
 </nav> 
+
+<div class="collapseButton">
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <div class="container-fluid">
+        <button type="button" id="sidebarCollapse" class="btn btn-info">
+            <i class="fa fa-bars"></i>
+        </button>
+    </div>
+</nav>
+</div>
 
 <div class="main">
 
@@ -76,10 +48,6 @@
 <h3>Northcliffe Badminton Club</h3>
 <p>We practise every Thursday night at the CYP. Join us at 8.00pm any Thursday. If you want to contact someone first, contact John on 01262 or email john@</p>
 
-<h3>High Flyers Badminton Club</h3>
-<p>The only club dedicated to younger players from 2-16 and we also have adult teams in the league.
- We practise every night at East Riding Leisure Bridlington on Promenade. Contact John on 01262 or email john@</p>
-
 <h3>BBC Badminton Club</h3>
 <p>We practise every night at East Riding Leisure Bridlington on Promenade. Contact John on 01262 or email john@</p>
 
@@ -88,9 +56,18 @@
 
 <h3>Scarborough Badminton Club</h3>
 <p>We practise every night at . Contact John on 01262 or email john@</p>
+
+<h3>The Yorkshire League</h3>
+<p>Are you ready for the next level? We enter teams into the Yorkshire League, where we currently have one team playing in Division 2 and one team playing in Division 3. Joining one of our local teams could set you on the path to playing at Yorkshire County level.<p>The Caption is Paul Robinson, contact Paul on 07952 915013.</p>
   
 </div>
 
+
+
+
+<div class="bottomFooter" id="bottomFooter"></div>
+
+</div>
 </div>
 
 </body>
